755c4389f8f8080bb466ba9940e0f67190610450
SelinERTEM <30592744+SelinERTEM@users.noreply.github.com>
2017-07-31 15:57:19 +0300

    haberSistemi

---
+++ HaberSistemi/HaberSistemi.Admin/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using HaberSistemi.Core.Infrastructure;
+using HaberSistemi.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaberSistemi.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region Kullanıcı
+        private readonly IKullaniciRepository _kullaniciRepository;
+        public AccountController(IKullaniciRepository kullaniciRepository)
+        {
+            _kullaniciRepository = kullaniciRepository;
+        }
+        #endregion
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Kullanici kullanici)
+        {
+            var KullaniciVarmi = _kullaniciRepository.GetMany(x => x.Email == kullanici.Email && x.Sifre == kullanici.Sifre && x.AktifMi == true).SingleOrDefault();
+            if (KullaniciVarmi != null)
+            {
+                if (KullaniciVarmi.Rol.RolAdi == "Admin")
+                {
+                    Session["KullaniciEmail"] = KullaniciVarmi.ID;
+                    return RedirectToAction("Index","Home");
+                }
+                ViewBag.Mesaj = "Yetkisiz Kullanıcı";
+                return View();
+            }
+            ViewBag.Mesaj = "Kullanıcı Bulunamadı";
+            return View();
+        }
+	}
+}
